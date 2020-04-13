7124bef4e9f5c4b2446f09436dd91d97e3735fca
rabin gurung <rabin1233@gmail.com>
2020-04-13 00:01:45 -0700

    Accout java updated

---
+++ leetCodes/src/stringSoln/Account.java
@@ -0,0 +1,27 @@
+package stringSoln;
+
+public class Account extends Deposit {
+
+	public Account(String name1, double d) {
+		String name = "rabin";
+		double d1 = 100;
+		// TODO Auto-generated constructor stub
+		return;
+	}
+
+	public void deposit(double depositAmount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static Object getName( String name) {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	public static Object getBalance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- leetCodes/src/stringSoln/Deposit.java
+++ leetCodes/src/stringSoln/Deposit.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 
 public class Deposit {
 	private String Account; 
+	private static String name1;
+	private static String name2;
     public static void main(String[]args) {
-        Account A1 = new Account("Jane Green", 100.00);
-        Account A2 = new Account("John Blue", -2.49);
+    	Scanner input = new Scanner(System.in);
+    	String name1 = input.next();
+    	String name2 = input.next();
+        Account A1 = new Account(name1, 100.00);
+        Account A2 = new Account(name2, -2.49);
         displayAccount(A1);
         displayAccount(A2);
         
-        Scanner input = new Scanner(System.in);
         System.out.print("amount deposited for account1:");
         
         double depositAmount = input.nextDouble();
@@ -31,6 +35,24 @@ public class Deposit {
     public static void displayAccount
     (Account accountToDisplay) {
         
-        System.out.printf("%s balance: $%.2f\n", accountToDisplay.getName(), accountToDisplay.getBalance());
+        System.out.printf("%s balance: $%.2f\n", accountToDisplay.getName(name1), accountToDisplay.getBalance());
+        System.out.printf("%s balance: $%.2f\n", accountToDisplay.getName(name2), accountToDisplay.getBalance());
     }
+
+
+	public static String getName1() {
+		return name1;
+	}
+
+	public static void setName1(String name1) {
+		Deposit.name1 = name1;
+	}
+	
+	public static String getName2() {
+		return name2;
+	}
+
+	public static void setName2(String name2) {
+		Deposit.name2 = name2;
+	}
 }
